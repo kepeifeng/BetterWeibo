5dd62162162379fd3c2cbfc32b3abc1290c00e4e
Kent <kepeifeng@gmail.com>
2013-09-28 21:28:04 +0800

    Initial Commit

---
+++ BetterWeibo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BetterWeibo
+//
+//  Created by Kent Peifeng Ke on 13-9-28.
+//  Copyright (c) 2013年 Kent Peifeng Ke. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ BetterWeibo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BetterWeibo
+//
+//  Created by Kent Peifeng Ke on 13-9-28.
+//  Copyright (c) 2013年 Kent Peifeng Ke. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
